200eca62eb2767ef24cd7aac7279918da30e1a4e
Timothy Whittaker <tdw419@gmail.com>
2026-03-02 13:05:25 -0600

    chore(open-brain): add requirements and finalize package

--- open_brain/__init__.py
+++ open_brain/__init__.py
@@ -1,5 +1,8 @@
 """Visual Open Brain - Geometry OS + Persistent Memory Integration."""
 __version__ = "0.1.0"
+
 from .memory_glyph import MemoryGlyphEncoder
 from .db import Database
-__all__ = ["MemoryGlyphEncoder", "Database"]
+from .visual_mcp import VisualMCPServer, create_server
+
+__all__ = ["MemoryGlyphEncoder", "Database", "VisualMCPServer", "create_server"]
